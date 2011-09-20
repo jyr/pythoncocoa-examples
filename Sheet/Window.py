8941e1d27d98484c66bace6eac484271cdf2549d
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-09-20 16:13:28 -0500

    example sheet

--- Sheet/Window.py
+++ Sheet/Window.py
@@ -22,6 +22,5 @@ class Window (NSWindow):
     @objc.IBAction
     def showSheet_(self, sender):
 		NSApp.beginSheet_modalForWindow_modalDelegate_didEndSelector_contextInfo_(self.sheet, self.window, self, None, None)
-		#NSApp.runModalForWindow_(self.sheet)
-		#NSApp.endSheet_(self.sheet)
+
 
